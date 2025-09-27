2fb2d38773b6c5d346bb4df344fb4c6d06c32366
Brayden Strivens <bdstrivens@gmail.com>
2025-09-27 15:00:38 -0500

    Updated login and registration views. Updates user profile view

---
+++ SkatePediaV2/Account/SubViews/UserPostPreviewCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserPostPreviewCell.swift
+//  SkatePediaV2
+//
+//  Created by Brayden Strivens on 9/26/25.
+//
+
+import SwiftUI
+
+struct UserPostPreviewCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserPostPreviewCell()
+}
---
+++ SkatePediaV2/Account/SubViews/UserPostsPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  UserPostsPreviewView.swift
+//  SkatePediaV2
+//
+//  Created by Brayden Strivens on 9/26/25.
+//
+
+import SwiftUI
+
+struct UserPostsPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserPostsPreviewView()
+}
